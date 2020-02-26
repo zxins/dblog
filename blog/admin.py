1432b9bf69ccdabddc5bdfef3556e93de40a4d5c
zhuxin <624042309@qq.com>
2020-02-26 23:32:38 +0800

    custom background

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Tag, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'body', 'excerpt', 'category', 'tag']
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 class Category(models.Model):
@@ -14,8 +16,13 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.name
+
 
 class Tag(models.Model):
     """
@@ -24,23 +31,27 @@ class Tag(models.Model):
    """
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.name
 
-class Post(models.Model):
 
+class Post(models.Model):
     # 标题
-    title = models.CharField(max_length=70)
+    title = models.CharField('标题', max_length=70)
 
     # 正文 TextField 来存储大段文本
-    body = models.TextField()
+    body = models.TextField('正文')
 
     # 创建时间&最后修改时间
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField('创建时间', default=timezone.now())
+    modified_time = models.DateTimeField('修改时间')
 
     # 文章摘要 可以没有摘要, 因此默认blank=True
-    excerpt = models.CharField(max_length=200, blank=True)
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
 
     # 分类 标签
     # 文章只能对应一个分类，但是一个分类下可以有多篇文章
@@ -48,8 +59,8 @@ class Post(models.Model):
     # 一篇文章可以有多个标签，同一个标签下也可能有多篇文章
     # ManyToManyField，表明这是多对多的关联关系
     # 也可以没有标签, 所以blank=True
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tag = models.ManyToManyField(Tag, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
 
     # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
     # django.contrib.auth 是 django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是
@@ -57,7 +68,15 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和
     # Category 类似。
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name  # 复数形式
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
