2fe3dd292f1d6af14a2e46ea8205b26769323953
zhuxin <624042309@qq.com>
2020-02-26 22:20:43 +0800

    update blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    """
+    django 要求模型必须继承 models.Model 类。
+    Category 只需要一个简单的分类名 name 就可以了。
+    CharField 指定了分类名 name 的数据类型，CharField 是字符型，
+    CharField 的 max_length 参数指定其最大长度，超过这个长度的分类名就不能被存入数据库。
+    当然 django 还为我们提供了多种其它的数据类型，如日期时间类型 DateTimeField、整数类型 IntegerField 等等。
+    django 内置的全部类型可查看文档：
+    https://docs.djangoproject.com/en/2.2/ref/models/fields/#field-types
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    """
+   标签 Tag 也比较简单，和 Category 一样。
+   再次强调一定要继承 models.Model 类！
+   """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+
+    # 标题
+    title = models.CharField(max_length=70)
+
+    # 正文 TextField 来存储大段文本
+    body = models.TextField()
+
+    # 创建时间&最后修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    # 文章摘要 可以没有摘要, 因此默认blank=True
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    # 分类 标签
+    # 文章只能对应一个分类，但是一个分类下可以有多篇文章
+    # models.CASCADE 参数，意为级联删除
+    # 一篇文章可以有多个标签，同一个标签下也可能有多篇文章
+    # ManyToManyField，表明这是多对多的关联关系
+    # 也可以没有标签, 所以blank=True
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
+    # django.contrib.auth 是 django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是
+    # django 为我们已经写好的用户模型。
+    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和
+    # Category 类似。
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
