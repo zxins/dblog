1423fd123bb38693efa0c185e0387f10e8e27a71
zhuxin <624042309@qq.com>
2020-02-26 23:00:24 +0800

    add blog templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页'
+        'post_list': post_list
     })
