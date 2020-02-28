75a773bad349ab00c98400425aca03b415dcdbee
zhuxin <624042309@qq.com>
2020-02-28 14:59:24 +0800

    fix bug

--- dblog/wsgi.py
+++ dblog/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dblog.settings')
-os.environ.setdefault('dblog.settings.production', 'dblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dblog.settings.production')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dblog.settings')
-    os.environ.setdefault('dblog.settings.local', 'dblog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dblog.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
