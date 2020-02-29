b185853923b1ddd8180922edfeac066109f7deeb
zhuxin <624042309@qq.com>
2020-02-29 20:07:48 +0800

    fix bug

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from .models import Category, Tag, Post
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
-    fields = ['title', 'body', 'excerpt', 'category', 'tag']
+    fields = ['title', 'body', 'excerpt', 'category', 'tags']
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user
