c64fbebdd94bb711eb8bd17fb3d66cc66fbace35
zhuxin <624042309@qq.com>
2020-02-29 19:06:51 +0800

    fix fabfile bu

--- fabfile.py
+++ fabfile.py
@@ -38,9 +38,9 @@ def deploy(c):
 
     # 安装依赖，迁移数据库，收集静态文件
     with c.cd(project_root_path):
-        c.run('pipenv install --deploy --ignore-pipfile')
-        c.run('pipenv run python manage.py migrate')
-        c.run('pipenv run python manage.py collectstatic --noinput')
+        c.run('sudo pipenv install --deploy --ignore-pipfile')
+        c.run('sudo pipenv run python manage.py migrate')
+        c.run('sudo pipenv run python manage.py collectstatic --noinput')
 
     # 重新启动应用
     with c.cd(supervisor_conf_path):
